e1610b0dd1fde2148d834fe146970e5fcf496de4
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-04 22:47:20 +0500

    added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// //OOP
+// console.log("//encapsulation\\");
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -13,80 +15,207 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-//OOP
-console.log("//encapsulation\\");
-var Laptop = /** @class */ (function () {
-    function Laptop(brand, year, ssd, id) {
-        this.brand = brand;
-        this.id = id;
-        this.ssd = ssd;
-        this.year = year;
+// class Laptop {
+//   readonly brand: string;
+//   year: number;
+//   ssd: number;
+//   private id: number;
+//   constructor(brand: string, year: number, ssd: number, id: number) {
+//     this.brand = brand;
+//     this.id = id;
+//     this.ssd = ssd;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Laptop : ${this.brand}  ${this.id} ${this.year}  ${this.ssd}`;
+//   }
+// }
+// let asus = new Laptop("Asus", 2024, 225, 1);
+// console.log(asus.getDetails());
+// class Car {
+//   brand: string;
+//   year: number;
+//   public distance: number;
+//   private color: string;
+//   constructor(brand: string, year: number, distance: number, color: string) {
+//     this.brand = brand;
+//     this.color = color;
+//     this.distance = distance;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Car : ${this.brand}  ${this.color} ${this.year}  ${this.distance}`;
+//   }
+// }
+// let Nexia = new Car("Nexia", 2024, 2500, "black");
+// console.log(Nexia.getDetails());
+// class Phone {
+//   brand: string;
+//   year: number;
+//   public price: number;
+//   private color: string;
+//   constructor(brand: string, year: number, price: number, color: string) {
+//     this.brand = brand;
+//     this.color = color;
+//     this.price = price;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Phone : ${this.brand}  ${this.color} ${this.year}  $${this.price}`;
+//   }
+// }
+// let Samsung = new Phone("Samsung", 2024, 200, "black");
+// console.log(Samsung.getDetails());
+// class Book {
+//   name: string;
+//   author: string;
+//   public paper: number;
+//   private price: number;
+//   constructor(name: string, author: string, paper: number, price: number) {
+//     this.name = name;
+//     this.price = price;
+//     this.paper = paper;
+//     this.author = author;
+//   }
+//   getDetails() {
+//     return `Book : ${this.name} ${this.price} so'm ${this.author}  ${this.paper}varoq`;
+//   }
+// }
+// let OtkanKunlar = new Book("O`tkan Kunlar", "o`tkir Hoshimov", 250, 2500);
+// console.log(OtkanKunlar.getDetails());
+// console.log('//abstracting\\');
+// abstract class Payment {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getPayment(): string;
+// }
+// class CreditPayment extends Payment {
+//   getPayment(): string {
+//     return `Amount: ${this.price}`;
+//   }
+// }
+// let pay = new CreditPayment(230);
+// console.log(pay.getPayment());
+// abstract class Person {
+//   age: number;
+//   constructor(age: number) {
+//     this.age = age;
+//   }
+//   abstract getPerson(): string;
+// }
+// class Iskandar extends Person {
+//   getPerson(): string {
+//     return `Age : ${this.age}`;
+//   }
+// }
+// let iskandar = new Iskandar(17);
+// console.log(iskandar.getPerson());
+// abstract class Home {
+//   kv: number;
+//   constructor(kv: number) {
+//     this.kv = kv;
+//   }
+//   abstract getHome(): string;
+// }
+// class HonSaroy extends Home {
+//   getHome(): string {
+//     return `kv : ${this.kv}`;
+//   }
+// }
+// let honSaroy:HonSaroy = new HonSaroy(3450);
+// console.log(honSaroy.getHome());
+// abstract class Apple {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getApple(): string;
+// }
+// class Iphone extends Apple {
+//   getApple(): string {
+//     return `price : ${this.price} $`;
+//   }
+// }
+// let iphone: Iphone = new Iphone(1340);
+// console.log(iphone.getApple());
+// console.log("//polymorphism\\");
+// abstract class Payment {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getPayment(): string;
+// }
+// class CreditPayment extends Payment {
+//   getPayment(): string {
+//     return `Credit Payment - Amount: ${this.price}`;
+//   }
+// }
+// class PayPalPayment extends Payment {
+//   getPayment(): string {
+//     return `PayPal Payment - Amount: ${this.price}`;
+//   }
+// }
+// class CashPayment extends Payment {
+//   getPayment(): string {
+//     return `Cash Payment - Amount: ${this.price}`;
+//   }
+// }
+// function processPayment(payment: Payment) {
+//   console.log(payment.getPayment());
+// }
+// let creditPay = new CreditPayment(230);
+// let paypalPay = new PayPalPayment(150);
+// let cashPay = new CashPayment(100);
+// processPayment(creditPay);
+// processPayment(paypalPay);
+// processPayment(cashPay);
+console.log("//inheritance\\");
+var Animal = /** @class */ (function () {
+    function Animal(name, isPredator) {
+        this.isPredator = isPredator;
+        this.name = name;
     }
-    Laptop.prototype.getDetails = function () {
-        return "Laptop : ".concat(this.brand, "  ").concat(this.id, " ").concat(this.year, "  ").concat(this.ssd);
-    };
-    return Laptop;
+    return Animal;
 }());
-var asus = new Laptop("Asus", 2024, 225, 1);
-console.log(asus.getDetails());
-var Car = /** @class */ (function () {
-    function Car(brand, year, distance, color) {
-        this.brand = brand;
-        this.color = color;
-        this.distance = distance;
-        this.year = year;
+var WildAnimal = /** @class */ (function (_super) {
+    __extends(WildAnimal, _super);
+    function WildAnimal(name, isPredator, age) {
+        var _this = _super.call(this, name, isPredator) || this;
+        _this.age = age;
+        return _this;
     }
-    Car.prototype.getDetails = function () {
-        return "Car : ".concat(this.brand, "  ").concat(this.color, " ").concat(this.year, "  ").concat(this.distance);
-    };
+    return WildAnimal;
+}(Animal));
+var lion = new WildAnimal("Alex", true, 5);
+console.log(lion);
+var HomeAnimal = /** @class */ (function (_super) {
+    __extends(HomeAnimal, _super);
+    function HomeAnimal(name, isPredator, age) {
+        var _this = _super.call(this, name, isPredator) || this;
+        _this.age = age;
+        return _this;
+    }
+    return HomeAnimal;
+}(Animal));
+var dog = new HomeAnimal("Kuchuk", true, 13);
+console.log(dog);
+var Car = /** @class */ (function () {
+    function Car(brand) {
+        this.brand = brand;
+    }
     return Car;
 }());
-var Nexia = new Car("Nexia", 2024, 2500, "black");
-console.log(Nexia.getDetails());
-var Phone = /** @class */ (function () {
-    function Phone(brand, year, price, color) {
-        this.brand = brand;
-        this.color = color;
-        this.price = price;
-        this.year = year;
+var NewCar = /** @class */ (function (_super) {
+    __extends(NewCar, _super);
+    function NewCar(brand, year) {
+        var _this = _super.call(this, brand) || this;
+        _this.year = year;
+        return _this;
     }
-    Phone.prototype.getDetails = function () {
-        return "Phone : ".concat(this.brand, "  ").concat(this.color, " ").concat(this.year, "  $").concat(this.price);
-    };
-    return Phone;
-}());
-var Samsung = new Phone("Samsung", 2024, 200, "black");
-console.log(Samsung.getDetails());
-var Book = /** @class */ (function () {
-    function Book(name, author, paper, price) {
-        this.name = name;
-        this.price = price;
-        this.paper = paper;
-        this.author = author;
-    }
-    Book.prototype.getDetails = function () {
-        return "Book : ".concat(this.name, " ").concat(this.price, " so'm ").concat(this.author, "  ").concat(this.paper, "varoq");
-    };
-    return Book;
-}());
-var OtkanKunlar = new Book("O`tkan Kunlar", "o`tkir Hoshimov", 250, 2500);
-console.log(OtkanKunlar.getDetails());
-//abstracting
-var Payment = /** @class */ (function () {
-    function Payment(price) {
-        this.price = price;
-    }
-    return Payment;
-}());
-var CreditPayment = /** @class */ (function (_super) {
-    __extends(CreditPayment, _super);
-    function CreditPayment() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    CreditPayment.prototype.getPayment = function () {
-        return "Amount: ".concat(this.price);
-    };
-    return CreditPayment;
-}(Payment));
-var pay = new CreditPayment(230);
-console.log(pay.getPayment());
+    return NewCar;
+}(Car));
+var bmw = new NewCar("BMW", 2023);
+console.log(NewCar);
--- app.ts
+++ app.ts
@@ -1,101 +1,242 @@
-//OOP
-console.log("//encapsulation\\");
+// //OOP
+// console.log("//encapsulation\\");
 
-class Laptop {
-  readonly brand: string;
-  year: number;
-  ssd: number;
-  private id: number;
-  constructor(brand: string, year: number, ssd: number, id: number) {
-    this.brand = brand;
-    this.id = id;
-    this.ssd = ssd;
-    this.year = year;
-  }
-  getDetails() {
-    return `Laptop : ${this.brand}  ${this.id} ${this.year}  ${this.ssd}`;
+// class Laptop {
+//   readonly brand: string;
+//   year: number;
+//   ssd: number;
+//   private id: number;
+//   constructor(brand: string, year: number, ssd: number, id: number) {
+//     this.brand = brand;
+//     this.id = id;
+//     this.ssd = ssd;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Laptop : ${this.brand}  ${this.id} ${this.year}  ${this.ssd}`;
+//   }
+// }
+
+// let asus = new Laptop("Asus", 2024, 225, 1);
+
+// console.log(asus.getDetails());
+
+// class Car {
+//   brand: string;
+//   year: number;
+//   public distance: number;
+//   private color: string;
+//   constructor(brand: string, year: number, distance: number, color: string) {
+//     this.brand = brand;
+//     this.color = color;
+//     this.distance = distance;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Car : ${this.brand}  ${this.color} ${this.year}  ${this.distance}`;
+//   }
+// }
+
+// let Nexia = new Car("Nexia", 2024, 2500, "black");
+
+// console.log(Nexia.getDetails());
+
+// class Phone {
+//   brand: string;
+//   year: number;
+//   public price: number;
+//   private color: string;
+//   constructor(brand: string, year: number, price: number, color: string) {
+//     this.brand = brand;
+//     this.color = color;
+//     this.price = price;
+//     this.year = year;
+//   }
+//   getDetails() {
+//     return `Phone : ${this.brand}  ${this.color} ${this.year}  $${this.price}`;
+//   }
+// }
+
+// let Samsung = new Phone("Samsung", 2024, 200, "black");
+
+// console.log(Samsung.getDetails());
+
+// class Book {
+//   name: string;
+//   author: string;
+//   public paper: number;
+//   private price: number;
+//   constructor(name: string, author: string, paper: number, price: number) {
+//     this.name = name;
+//     this.price = price;
+//     this.paper = paper;
+//     this.author = author;
+//   }
+//   getDetails() {
+//     return `Book : ${this.name} ${this.price} so'm ${this.author}  ${this.paper}varoq`;
+//   }
+// }
+
+// let OtkanKunlar = new Book("O`tkan Kunlar", "o`tkir Hoshimov", 250, 2500);
+
+// console.log(OtkanKunlar.getDetails());
+
+// console.log('//abstracting\\');
+
+// abstract class Payment {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getPayment(): string;
+// }
+
+// class CreditPayment extends Payment {
+//   getPayment(): string {
+//     return `Amount: ${this.price}`;
+//   }
+// }
+
+// let pay = new CreditPayment(230);
+// console.log(pay.getPayment());
+
+// abstract class Person {
+//   age: number;
+//   constructor(age: number) {
+//     this.age = age;
+//   }
+//   abstract getPerson(): string;
+// }
+
+// class Iskandar extends Person {
+//   getPerson(): string {
+//     return `Age : ${this.age}`;
+//   }
+// }
+
+// let iskandar = new Iskandar(17);
+// console.log(iskandar.getPerson());
+
+// abstract class Home {
+//   kv: number;
+//   constructor(kv: number) {
+//     this.kv = kv;
+//   }
+//   abstract getHome(): string;
+// }
+
+// class HonSaroy extends Home {
+//   getHome(): string {
+//     return `kv : ${this.kv}`;
+//   }
+// }
+
+// let honSaroy:HonSaroy = new HonSaroy(3450);
+// console.log(honSaroy.getHome());
+
+// abstract class Apple {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getApple(): string;
+// }
+
+// class Iphone extends Apple {
+//   getApple(): string {
+//     return `price : ${this.price} $`;
+//   }
+// }
+
+// let iphone: Iphone = new Iphone(1340);
+// console.log(iphone.getApple());
+
+// console.log("//polymorphism\\");
+
+// abstract class Payment {
+//   price: number;
+//   constructor(price: number) {
+//     this.price = price;
+//   }
+//   abstract getPayment(): string;
+// }
+
+// class CreditPayment extends Payment {
+//   getPayment(): string {
+//     return `Credit Payment - Amount: ${this.price}`;
+//   }
+// }
+
+// class PayPalPayment extends Payment {
+//   getPayment(): string {
+//     return `PayPal Payment - Amount: ${this.price}`;
+//   }
+// }
+
+// class CashPayment extends Payment {
+//   getPayment(): string {
+//     return `Cash Payment - Amount: ${this.price}`;
+//   }
+// }
+
+// function processPayment(payment: Payment) {
+//   console.log(payment.getPayment());
+// }
+
+// let creditPay = new CreditPayment(230);
+// let paypalPay = new PayPalPayment(150);
+// let cashPay = new CashPayment(100);
+
+// processPayment(creditPay);
+// processPayment(paypalPay);
+// processPayment(cashPay);
+
+console.log("//inheritance\\");
+class Animal {
+  name: string;
+  isPredator: boolean;
+  constructor(name: string, isPredator: boolean) {
+    this.isPredator = isPredator;
+    this.name = name;
   }
 }
 
-let asus = new Laptop("Asus", 2024, 225, 1);
+class WildAnimal extends Animal {
+  age: number;
+  constructor(name: string, isPredator: boolean, age: number) {
+    super(name, isPredator);
+    this.age = age;
+  }
+}
 
-console.log(asus.getDetails());
+let lion: WildAnimal = new WildAnimal("Alex", true, 5);
+console.log(lion);
+
+class HomeAnimal extends Animal {
+  age: number;
+  constructor(name: string, isPredator: boolean, age: number) {
+    super(name, isPredator);
+    this.age = age;
+  }
+}
+
+let dog: HomeAnimal = new HomeAnimal("Kuchuk", true, 13);
+console.log(dog);
 
 class Car {
   brand: string;
-  year: number;
-  public distance: number;
-  private color: string;
-  constructor(brand: string, year: number, distance: number, color: string) {
+  constructor(brand: string) {
     this.brand = brand;
-    this.color = color;
-    this.distance = distance;
+  }
+}
+
+class NewCar extends Car {
+  year: number;
+  constructor(brand: string, year: number) {
+    super(brand);
     this.year = year;
   }
-  getDetails() {
-    return `Car : ${this.brand}  ${this.color} ${this.year}  ${this.distance}`;
-  }
 }
 
-let Nexia = new Car("Nexia", 2024, 2500, "black");
-
-console.log(Nexia.getDetails());
-
-class Phone {
-  brand: string;
-  year: number;
-  public price: number;
-  private color: string;
-  constructor(brand: string, year: number, price: number, color: string) {
-    this.brand = brand;
-    this.color = color;
-    this.price = price;
-    this.year = year;
-  }
-  getDetails() {
-    return `Phone : ${this.brand}  ${this.color} ${this.year}  $${this.price}`;
-  }
-}
-
-let Samsung = new Phone("Samsung", 2024, 200, "black");
-
-console.log(Samsung.getDetails());
-
-class Book {
-  name: string;
-  author: string;
-  public paper: number;
-  private price: number;
-  constructor(name: string, author: string, paper: number, price: number) {
-    this.name = name;
-    this.price = price;
-    this.paper = paper;
-    this.author = author;
-  }
-  getDetails() {
-    return `Book : ${this.name} ${this.price} so'm ${this.author}  ${this.paper}varoq`;
-  }
-}
-
-let OtkanKunlar = new Book("O`tkan Kunlar", "o`tkir Hoshimov", 250, 2500);
-
-console.log(OtkanKunlar.getDetails());
-
-//abstracting
-
-abstract class Payment {
-  price: number;
-  constructor(price: number) {
-    this.price = price;
-  }
-  abstract getPayment(): string; 
-}
-
-class CreditPayment extends Payment {
-  getPayment(): string {
-    return `Amount: ${this.price}`;
-  }
-}
-
-let pay = new CreditPayment(230); 
-console.log(pay.getPayment());
+let bmw: NewCar = new NewCar("BMW", 2023);
+console.log(NewCar);
